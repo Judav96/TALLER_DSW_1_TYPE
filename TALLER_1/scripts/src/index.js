7b527703d8fe978752e129aaca380d63b5fd1575
Julian Guevara <jd.guevarar12@uniandes.edu.co>
2025-11-01 11:20:59 -0500

    DEtalles menores

---
+++ TALLER_1/scripts/src/index.js
@@ -0,0 +1,43 @@
+import { series } from "./data.js";
+var seriesTableBody = document.getElementById("series-table-body");
+var averageElement = document.getElementById("average-seasons");
+var detailElement = document.getElementById("series-detail");
+function renderSeries(seriesList) {
+    seriesTableBody.innerHTML = "";
+    for (var i = 0; i < seriesList.length; i++) {
+        var s = seriesList[i];
+        var row = document.createElement("tr");
+        row.innerHTML =
+            "<td>" + s.id + "</td>" +
+                "<td><a href='#' id='link-" + s.id + "'>" + s.name + "</a></td>" +
+                "<td>" + s.channel + "</td>" +
+                "<td>" + s.seasons + "</td>";
+        seriesTableBody.appendChild(row);
+        (function (serie) {
+            var link = document.getElementById("link-" + serie.id);
+            link.onclick = function () {
+                showDetail(serie);
+                return false;
+            };
+        })(s);
+    }
+}
+function showDetail(serie) {
+    detailElement.innerHTML =
+        "<div class='card' style='width: 100%;'>" +
+            "<img src='" + serie.image + "' class='card-img-top' alt='" + serie.name + "'>" +
+            "<div class='card-body'>" +
+            "<h5 class='card-title'>" + serie.name + "</h5>" +
+            "<p class='card-text'>" + serie.description + "</p>" +
+            "<a href='" + serie.link + "' target='_blank'>Ver más</a>" +
+            "</div></div>";
+}
+function getAverageSeasons(seriesList) {
+    var total = 0;
+    for (var i = 0; i < seriesList.length; i++) {
+        total += seriesList[i].seasons;
+    }
+    return total / seriesList.length;
+}
+renderSeries(series);
+averageElement.textContent = getAverageSeasons(series).toFixed(2);
